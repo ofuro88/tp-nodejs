35af58113b5a11f7f80a6919197491a056d0e85e
ofuro88 <guiffan.florestan@gmail.com>
2018-03-06 15:25:07 +0100

    squelette de base du projet

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+// tableaux
+    // personnes
+
+    // groupes
+
+
+// middlewares
+
+
+// lectures
+
+
+// modification
+
+
+// suppression
+
+
+// lancement du serveur web
+app.listen(3000, () => console.log('On http://localhost:3000/'))

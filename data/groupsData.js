4689816c11decebbc0b8bb9a84cf11c56c16d775
ofuro88 <guiffan.florestan@gmail.com>
2018-03-15 11:51:33 +0100

    modifications des données de groups (erreur dans les id)

--- data/groupsData.js
+++ data/groupsData.js
@@ -4,7 +4,7 @@ let nextGroupId = 0
 module.exports ={
     groups : [
         { id: nextGroupId++, name: 'collaborator', members: [1, 2, 3, 4] },
-        { id: nextGroupId++, name: 'friends', members: [1, 4, 5, 6] }
+        { id: nextGroupId++, name: 'friends', members: [0, 1, 2, 5] }
     ],
     nextGroupId : nextGroupId
 }
